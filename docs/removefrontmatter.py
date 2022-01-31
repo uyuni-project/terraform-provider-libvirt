5d7716e1583a2b2ea4ab2c33c6f9f8722570fa95
Julio Gonzalez Gil <jgonzalez@suse.com>
2022-01-31 17:17:58 +0100

    Instruct users/devs to use upstream unles instructed otherwise

--- docs/removefrontmatter.py
+++
@@ -1,8 +0,0 @@
-import re
-
-# Remove the frint matter (YAML) from markdown files
-def source_read_handler(app, docname, source):
-    source[0] = re.sub(r'^---\n(.+\n)*---\n', '', source[0])
-
-def setup(app):
-    app.connect('source-read', source_read_handler)
